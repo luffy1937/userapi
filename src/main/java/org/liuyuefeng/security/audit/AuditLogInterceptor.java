5fb85692be3483ae1a9cc33e1cbe6c7f9d4cf6ba
liuyuefeng <liuyf935@chinaunicom.cn>
2020-08-01 09:38:34 +0800

    audit

--- src/main/java/org/liuyuefeng/security/audit/AuditLogInterceptor.java
+++ src/main/java/org/liuyuefeng/security/audit/AuditLogInterceptor.java
@@ -1,7 +1,39 @@
 package org.liuyuefeng.security.audit;
 
+import org.liuyuefeng.security.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class AuditLogInterceptor extends HandlerInterceptorAdapter {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@Component
+public class AuditLogInterceptor extends HandlerInterceptorAdapter {
+    @Autowired
+    private AuditLogRepository auditLogRepository;
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        AuditLog log = new AuditLog();
+        log.setMethod(request.getMethod());
+        log.setPath(request.getRequestURI());
+        User user = (User)request.getAttribute("user");
+        if(user != null){
+            log.setUsername(user.getUserName());
+        }
+        auditLogRepository.save(log);
+        request.setAttribute("auditLogId", log.getId());
+        return true;
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        Long auditLogId = (Long)request.getAttribute("auditLogId");
+        AuditLog auditLog = auditLogRepository.findById(auditLogId).get();
+        auditLog.setStatus(response.getStatus());
+        auditLogRepository.save(auditLog);
+    }
+
+    public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    }
 }
--- src/main/java/org/liuyuefeng/security/audit/AuditLogRepository.java
+++ src/main/java/org/liuyuefeng/security/audit/AuditLogRepository.java
@@ -1,6 +1,7 @@
 package org.liuyuefeng.security.audit;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
 
-public interface AuditLogRepository extends JpaSpecificationExecutor<AuditLog> {
+public interface AuditLogRepository extends JpaSpecificationExecutor<AuditLog>, CrudRepository<AuditLog, Long>{
 }
