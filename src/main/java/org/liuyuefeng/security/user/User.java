4ca19508026ba6ef4c8eb564cbf743391d8a6e74
liuyuefeng <liuyf935@chinaunicom.cn>
2020-08-01 16:03:58 +0800

    数据层非空校验

--- src/main/java/org/liuyuefeng/security/user/User.java
+++ src/main/java/org/liuyuefeng/security/user/User.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Entity
@@ -16,7 +17,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @NotBlank(message = "用户名不能为空")
     private String userName;
+    @NotBlank(message = "密码不能为空")
     private String password;
     public UserInfo buildInfo(){
         UserInfo userInfo = new UserInfo();
